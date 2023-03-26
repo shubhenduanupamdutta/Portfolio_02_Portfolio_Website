0375801eb1cbf604dfda82c7e0785475288fcc76
Shubhendu <shubhenduanupam@gmail.com>
2023-03-26 20:05:28 +0530

    Contact form functional

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, url_for
 from flask_bootstrap import Bootstrap5
 from datetime import datetime
 from forms import *
+import smtplib
+from email.message import EmailMessage
 import os
 
+# Mail ID and Password from environment
+EMAIL_ID = os.environ["EMAIL"]
+PASSWORD = os.environ["PASSWORD"]
+TO_EMAIL = os.environ["EMAIL_TO"]
 # Starting Flask App
 app = Flask(__name__)
 app.secret_key = os.environ["SECRET_KEY"]
@@ -14,7 +20,16 @@ bootstrap.init_app(app)
 
 
 def send_mail(name, email, subject, message):
-    pass
+    with smtplib.SMTP("smtp.office365.com", port=587) as connection:
+        connection.starttls()
+        connection.login(EMAIL_ID, PASSWORD)
+        msg = EmailMessage()
+        email_body = f"From: {name}\nEmail: {email}\nSubject: {subject}\nMessage: {message}"
+        msg.set_content(email_body)
+        msg["Subject"] = "You have a new mail from your portfolio website."
+        msg["To"] = TO_EMAIL
+        msg["From"] = EMAIL_ID
+        connection.send_message(msg)
 
 
 @app.route('/', methods=["GET", "POST"])
