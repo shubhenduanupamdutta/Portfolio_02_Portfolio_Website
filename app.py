8d898a58611afcdc277786f15581465cb2f4d782
Shubhendu <shubhenduanupam@gmail.com>
2023-03-26 13:28:07 +0530

    Create Introduction Section

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap5
+from datetime import datetime
+import os
 
+# Starting Flask App
 app = Flask(__name__)
+app.secret_key = os.environ["SECRET_KEY"]
+
+# Initializing Flask Bootstrap
+bootstrap = Bootstrap5()
+bootstrap.init_app(app)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return render_template("index.html")
+
+
+@app.context_processor
+def inject_variables():
+    cur_year = datetime.now().year
+    return dict(year=cur_year)
 
 
 if __name__ == '__main__':
